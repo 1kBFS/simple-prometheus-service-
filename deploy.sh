a1f42a0081da4d9b584ecb8392080c22c148251c
= <=>
2024-05-22 21:09:39 +0300

    fix: fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-pip3 install uwsgi
-uwsgi --http 0.0.0.0:8080 --wsgi-file app.py --callable app
+nohup uwsgi --http 0.0.0.0:8080 --wsgi-file app.py --callable app > uwsgi.log 2>&1 &
+echo "Deployed"
